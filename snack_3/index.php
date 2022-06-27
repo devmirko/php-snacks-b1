725038e4e372c4fcf67b798b70f33b2fccdf8350
mirkosimonetti <mirko.simonetti.ta@gmail.com>
2022-06-27 17:50:46 +0200

    fine esercizio

--- snack_3/index.php
+++ snack_3/index.php
@@ -1,1 +1,76 @@
-<!-- Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. Stampare ogni data con i relativi post. -->
+<!-- Creare un array dj array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. Stampare ogni data con i relativi post. -->
+
+<?php
+
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => '$posts',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+   <h1>Post del giorno</h1>
+   <?php 
+   $keys = array_keys($posts);
+   for ($i=0; $i < count($posts); $i++) { 
+        $date = $posts[$keys[$i]];
+        echo  "<div> {$keys[$i]} </div>";
+        for ($j=0; $j <  count($date); $j++) { 
+            $post = $date[$j];
+            echo  "<div> {$post['author']} </div>";
+            echo  "<div> {$post['title']} </div>";
+            echo  "<p> {$post['text']} </p>";
+
+        }
+
+   }
+    ?>
+
+    
+</body>
+</html>
--- snack_4/index.php
+++ snack_4/index.php
@@ -1,1 +1,35 @@
 <!-- Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta -->
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+
+<body>
+    <div>
+
+        <?php
+        $nMax = 100;
+        $nMin = 0;
+
+        $numbersArray = [];
+
+        while (count($numbersArray) < 15) {
+            $randomNumber = mt_rand($nMin, $nMax);
+
+            if (!in_array($randomNumber, $numbersArray)) {
+                $numbersArray[] = $randomNumber;
+            }
+        }
+
+        echo '<pre>' . var_dump($numbersArray) . '</pre>';
+        ?>
+
+    </div>
+</body>
+
+</html>
